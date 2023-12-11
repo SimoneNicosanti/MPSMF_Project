bc40a437c996f03bb15cdfbcb80a55707ec1a8ae
EdoMan000 <manenti000@gmail.com>
2023-12-11 15:52:58 +0100

    pushing changes
    Co-authored-by: EdmondDantes <SimoneNicosanti@users.noreply.github.com>

--- R/R_Imports.R
+++ R/R_Imports.R
@@ -1,5 +1,8 @@
 # Install and load the strucchange package
 install.packages("strucchange", repos = "https://cran.mirror.garr.it/CRAN/")
 install.packages("SupMZ", repos = "https://cran.mirror.garr.it/CRAN/")
+install.packages("tseries", repos = "https://cran.mirror.garr.it/CRAN/")
+
 library(strucchange)
 library(SupMZ)
+library(tseries)
--- R/R_Scripts.R
+++ R/R_Scripts.R
@@ -15,3 +15,13 @@ supmzBreakpointsSearch <- function(time_series) {
     test_result <- supmz(formula = LogClosePrice ~ 1, data = time_series, nBoot = 10)
     return(test_result$BreakLocation)
 }
+
+garchResiduals <- function(time_series) {
+    model <- garch(time_series, order=c(1,1), series=NULL, coef=NULL, maxiter=200,
+        grad="analytical", trace=TRUE, eps=NULL, 
+        abstol = max(1e-20, .Machine$double.eps^2),
+        reltol = max(1e-10, .Machine$double.eps^(2/3)), 
+        xtol = sqrt(.Machine$double.eps),
+        falsetol = 1e2 * .Machine$double.eps)
+    return(model$residuals)
+}
