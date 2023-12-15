d56eb1da1cca4537caf529a94a778eecc3524e07
EdmondDantes <snicosanti@gmail.com>
2023-12-15 20:35:20 +0100

    Merge e aggiunta commenti markdown... non funziona più niente...
    Co-authored-by: Edoardo Manenti <EdoMan000@users.noreply.github.com>

--- R/R_Skewt.R
+++ R/R_Skewt.R
@@ -1,14 +1,14 @@
 skewt_fit <- function(data) {
-    fitted_skewt <- fitdistrplus::fitdist(data, dskt, start=list(gamma = 1, df = 1), method= "mle")
+    fitted_skewt <- fitdistrplus::fitdist(data, dst, start=list(xi = 1, omega = 1, alpha = 1, nu = 1), method= "mle")
     summary(fitted_skewt)
     return_list <- list(params = fitted_skewt$estimate, loglik = fitted_skewt$loglik, summary = summary(fitted_skewt))
     return(return_list)
 }
 
-skewt_dist <- function(x_points, gamma, df) {
-    return(dskt(x_points, df, gamma = gamma))
+skewt_dist <- function(x_points, xi, omega, alpha, nu) {
+    return(dst(x_points, xi, omega, alpha, nu))
 }
 
-skewt_gen <- function(n, gamma, df) {
-    return(rskt(n, df, gamma))
+skewt_gen <- function(n, xi, omega, alpha, nu) {
+    return(rst(n, xi, omega, alpha, nu))
 }
